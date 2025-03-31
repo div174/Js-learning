129ea573d2309257d1b335a9fb75d86451f5e906
div174 <155170554+div174@users.noreply.github.com>
2025-03-31 16:09:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOpns.js
@@ -0,0 +1,33 @@
+// let score = "33ab"
+
+// console.log(typeof score);
+
+// let valInNum = Number(score)
+// console.log(typeof valInNum);
+// console.log(valInNum); //not a num NaN
+
+// "33" ->33
+// "33abc", undefined->NaN
+// null->0
+// true->1, false->0
+
+
+
+// let isLoggedIn = 1
+// console.log(typeof isLoggedIn);
+
+// isLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(isLoggedIn);
+
+// 0,""->false
+// 1,3,-2,"d",->true
+
+
+
+let num = 11
+console.log(typeof num);
+
+num = String(num)
+console.log(typeof num);
+console.log(num);
